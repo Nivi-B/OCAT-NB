527b530919f184505b35209ea2e148b57f4c6240
Nivedha Balasubramanian <balasuna@mail.uc.edu>
2022-10-04 16:05:26 -0400

    task 2

--- OCAT/client/pages/Assessments/AssessmentList.jsx
+++ OCAT/client/pages/Assessments/AssessmentList.jsx
@@ -20,74 +20,124 @@ export const AssessmentList = () => {
 
   console.log(assessments);
 
-  /*
-const data = React.useMemo(
-     () => [
-       {
-         col1: '',
-         col2: '',
-       },
-       {
-         col1: '',
-         col2: '',
-       },
-       {
-         col1: '',
-         col2: '',
-       },
-     ],
-     []
-   )
-
-   const columns = React.useMemo(
-     () => [
-       {
-         Header: '',
-         accessor: '',
-       },
-       {
-         Header: '',
-         accessor: '',
-       },
-     ],
-     []
-   )
-*/
-
-  return (
-    <div>
-      {/*
-          List goes here
-          Please use the library react-table https://www.npmjs.com/package/react-table
-           */
-
-        <table>
-
-          <thead>
-            <tr>
-              <th>cat name</th>
-              <th>cat date of birth</th>
-              <th>score</th>
-              <th>Risk level</th>
-              <th>date created</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {assessments.map(() =>
-              <tr>
-                <td>{assessments.cat_name}</td>
-                <td>{assessments.cat_date_of_birth}</td>
-                <td>{assessments.score}</td>
-                <td>{assessments.risk_level}</td>
-                <td>{assessments.created_at}</td>
-              </tr>)}
-
-          </tbody>
-
-        </table>
-
-      }
-    </div>
+  /* const data = React.useMemo(
+    () => [
+      {
+        col1: `Cat Name`,
+        col2: `cat_name`,
+      },
+      {
+        col1: `Cat Date of Birth`,
+        col2: `cat_date_of_birth`,
+      },
+      {
+        col1: `Score`,
+        col2: `score`,
+      },
+      {
+        col1: `Risk Level`,
+        col2: `risk_level`,
+      },
+      {
+        col1: `Date Created`,
+        col2: `created_at`,
+      },
+    ],
+    [],
   );
+  */
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: `Cat Name`,
+        accessor: `cat_name`,
+      },
+      {
+        Header: `Cat Date of Birth`,
+        accessor: `cat_date_of_birth`,
+      },
+      {
+        Header: `Score`,
+        accessor: `score`,
+      },
+      {
+        Header: `Risk Level`,
+        accessor: `risk_level`,
+      },
+      {
+        Header: `Date Created`,
+        accessor: `created_at`,
+      },
+    ],
+    [],
+  );
+
+  function Table({ data }) {
+    const {
+      getTableBodyProps,
+      getTableProps,
+      headerGroups,
+      prepareRow,
+      rows,
+    } = useTable({ columns, data });
+
+    return (
+      <div>
+        {
+          /* List goes here
+            Please use the library react-table https://www.npmjs.com/package/react-table */
+
+          <table {...getTableProps()} style={{ border: `solid 1px blue` }}>
+            <thead>
+              {headerGroups.map(headerGroup =>
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                  {headerGroup.headers.map(column =>
+                    <th
+                      {...column.getHeaderProps()}
+                      style={{
+                        borderBottom: `solid 3px red`,
+                        background: `aliceblue`,
+                        color: `black`,
+                        fontWeight: `bold`,
+                      }}
+                    >
+                      {column.render(`Header`)}
+                    </th>)}
+                </tr>)}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+              {rows.map(row => {
+                prepareRow(row);
+                return (
+                  <tr {...row.getRowProps()}>
+                    {row.cells.map(cell =>
+                      <td
+                        {...cell.getCellProps()}
+                        style={{
+                          padding: `10px`,
+                          border: `solid 1px gray`,
+                          background: `papayawhip`,
+                        }}
+                      >
+                        {cell.render(`Cell`)}
+                      </td>)}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        }
+      </div>
+
+    );
+  }
+  return (
+    <div className='Table'>
+
+      <h1><center>Risk Assessment for Cats</center></h1>
+      <Table columns={columns} data={assessments} />
+    </div>
+
+  );
+
 };
